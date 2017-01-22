a6dd78bd0ea65ccd6ea5940cdd8ab98ddb92b2d0
AsimoLoveGym <xiazhuangz@gmail.com>
2017-01-21 20:44:03 -0600

    [FEAT] Use Autoprefixer instead of Bourbon mixins

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,10 +3,13 @@
 var gulp = require('gulp');
 var responsive = require('gulp-responsive');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('./scss/**/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    // .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(gulp.dest('./css'));
 });
 
