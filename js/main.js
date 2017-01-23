7347f301405f5bcbac91cd2587ef2016ad7851f8
AsimoLoveGym <xiazhuangz@gmail.com>
2017-01-22 23:26:27 -0600

    [FEAT] Add scroll & show with css3 transition & jQuery addClass

--- js/main.js
+++ js/main.js
@@ -1,59 +1,24 @@
-// $('#nav').affix({
-//       offset: {
-//         top: $(window).height(),
-//       },
-//     });
-
-// /* activate sidebar */
-// $('#sidebar').affix({
-//   offset: {
-//     top: 235
-//   }
-// });
-//
-// /* activate scrollspy menu */
-// var $body   = $(document.body);
-// var navHeight = $('.navbar').outerHeight(true) + 10;
-//
-// $body.scrollspy({
-// 	target: '#leftCol',
-// 	offset: navHeight
-// });
-//
-// /* smooth scrolling sections */
-// $('a[href*=#]:not([href=#])').click(function() {
-//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//       if (target.length) {
-//         $('html,body').animate({
-//           scrollTop: target.offset().top - 50
-//         }, 1000);
-//         return false;
-//       }
-//     }
-// });
-
 $(document).ready(function () {
     // $('.scroll-show').each(function (i) {
     //   $(this).css('opacity', '0');
     // });
     //
-    // /* Every time the window is scrolled ... */
-    // $(window).scroll(function () {
-    //     /* Check the location of each desired element */
-    //     $('.scroll-show').each(function (i) {
-    //
-    //       // var bottomOfObject = $(this).offset().top + $(this).outerHeight();
-    //       var bottomOfObject = $(this).offset().top + $(this).outerHeight() / 3;
-    //       var bottomOfWindow = $(window).scrollTop() + $(window).height();
-    //
-    //       /* If the object is completely visible in the window, fade it it */
-    //       if (bottomOfWindow > bottomOfObject) {
-    //         $(this).animate({ 'opacity': '1' }, 800);
-    //       }
-    //     });
-    //   });
+    /* Every time the window is scrolled ... */
+    $(window).scroll(function () {
+        /* Check the location of each desired element */
+        $('.faded').each(function (i) {
+
+          // var bottomOfObject = $(this).offset().top + $(this).outerHeight();
+          var bottomOfObject = $(this).offset().top + $(this).outerHeight() / 2;
+          var bottomOfWindow = $(window).scrollTop() + $(window).height();
+
+          /* If the object is completely visible in the window, fade it it */
+          if (bottomOfWindow > bottomOfObject) {
+            // $(this).animate({ 'opacity': '1' }, 800);
+            $(this).addClass('scroll-show');
+          }
+        });
+      });
 
     $('#booklist').click(function () {
       $('#collapseBooklist').slideToggle(1500);
