e9049f83959b16bd2fe7dfc362409c6709c62e81
AsimoLoveGym <xiazhuangz@gmail.com>
2017-01-22 18:45:13 -0600

    [FEAT] Smooth scroll, navigation. Apply collapse for expanding booklist

--- js/main.js
+++ js/main.js
@@ -33,3 +33,35 @@
 //       }
 //     }
 // });
+
+$(document).ready(function () {
+  $('#booklist').click(function () {
+    console.log($(this).html());
+    $(this).text($(this).text() == 'View Booklist' ? 'Hide Booklist' : 'View Booklist');
+  });
+});
+
+// Add smooth scrolling on all links inside the navbar
+$('#navbar a, #firstpageintro, #backtotop, #checkmywork').on('click', function (event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== '') {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top,
+    }, 1000, function () {
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+
+  } // End if
+
+});
